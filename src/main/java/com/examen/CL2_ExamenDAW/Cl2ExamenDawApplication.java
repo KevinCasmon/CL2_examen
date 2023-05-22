0df662454320a0b128b1fb33f78dba13b598185b
KevinCasmon <castillomonzonkevin@gmail.com>
2023-05-22 17:07:46 -0500

    Primer commint

---
+++ src/main/java/com/examen/CL2_ExamenDAW/Cl2ExamenDawApplication.java
@@ -0,0 +1,13 @@
+package com.examen.CL2_ExamenDAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2ExamenDawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2ExamenDawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/CL2_ExamenDAW/Cl2ExamenDawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.CL2_ExamenDAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2ExamenDawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
